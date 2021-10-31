34c15f574dcb09c6af98fb08489ac777a1a2a919
chase <chase.hildebrand4@gmail.com>
2021-10-31 03:17:11 -0400

    Rename main file

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup
 setup(
     name='algotester',
     version='0.1.0',
-    #py_modules=['yourscript'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'algotester = test:cli',
+            'algotester = algotester:cli',
         ],
     },
 )
